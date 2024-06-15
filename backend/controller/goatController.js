bd9586550e7ff997b3ff6902b5e7f8ca8a36ff96
Aaditya8C <aadityajp0419@gmail.com>
2024-06-15 20:23:18 +0530

    ..

--- backend/controller/goatController.js
+++ backend/controller/goatController.js
@@ -1,90 +1,65 @@
-const Goat = require('../models/goatmodel')
+const Goat = require("../models/goatmodel");
 
-const getgoatinfo = async(req,res)=>
-    {
-        try
-        {
-            const goat = await Goat.find({})
-            res.send(200).json(
-                {
-                    success:true,
-                    message:'successfully fecthed goat into',
-                    goats:goat
-                }
-            )
-        }
-        catch(err)
-        {
-            console.log(err)
-            res.status(500).json(
-                {
-                    success:false,
-                    message:"error while fetching goat info"
-                }
-            )
-        }
-    }
+const getgoatinfo = async (req, res) => {
+  try {
+    const goat = await Goat.find({});
+    res.send(200).json({
+      success: true,
+      message: "successfully fecthed goat into",
+      goats: goat,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "error while fetching goat info",
+    });
+  }
+};
 
-const getgoatbyid=async(req,res)=>
-{
-    try
-    {
-        const {id}=req.body._id
-        const goat = await Goat.find({_id:id})
-        res.send(200).json(
-            {
-                success:true,
-                message:'successfully fecthed goat into',
-                goats:goat
-            }
-        )
-    }
-    catch(err)
-    {
-        console.log(err)
-        res.status(500).json(
-            {
-                success:false,
-                message:"error while fetching goat info"
-            }
-        )
-    }
-}
+const getgoatbyid = async (req, res) => {
+  try {
+    const { id } = req.body._id;
+    const goat = await Goat.find({ _id: id });
+    res.send(200).json({
+      success: true,
+      message: "successfully fecthed goat into",
+      goats: goat,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "error while fetching goat info",
+    });
+  }
+};
 
-const creategoatinfo = async(req,res)=>
-{
-    try
-    {
-        const {gender,tagno,weight,price,age,vaccinationdate,date} = req.body
-        const goat = new Goat(
-            {
-                gender,
-                tagno,
-                weight,
-                price,
-                age,
-                date,
-                vaccinationdate
-            }
-        )
-        const saveuser = await goat.save()
-        res.status(201).json(
-            {
-                success:true,
-                message:"successfully created goat"
-            }
-        )
-    }
-    catch(err)
-    {
-        console.log(err)
-        res.status(505).json(
-            {
-                message:"error while creating goat",
-                success:false
-            }
-        )
-    }
-}
+const creategoatinfo = async (req, res) => {
+  try {
+    const { gender, tagno, weight, price, age, vaccinationdate, date } =
+      req.body;
+    const goat = new Goat({
+      gender,
+      tagno,
+      weight,
+      price,
+      age,
+      date,
+      vaccinationdate,
+    });
+    const saveuser = await goat.save();
+    res.status(201).json({
+      success: true,
+      message: "successfully created goat",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(505).json({
+      message: "error while creating goat",
+      success: false,
+    });
+  }
+};
 
-module.exports = {getgoatinfo,getgoatbyid,creategoatinfo};
+module.exports = { getgoatinfo, getgoatbyid, creategoatinfo };
