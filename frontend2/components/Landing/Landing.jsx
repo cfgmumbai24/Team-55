6b769bc61d90fc0bfa5cd7a74c2dcc468549baba
GitKritiM <imkritimehrota@gmail.com>
2024-06-16 01:49:03 +0530

    Home

--- frontend2/components/Landing/Landing.jsx
+++ frontend2/components/Landing/Landing.jsx
@@ -6,7 +6,8 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-
+import { Img } from "@chakra-ui/react";
+import Image from 'next/image';
 const Landing = () => {
   useEffect(() => {
     AOS.init();
@@ -45,24 +46,9 @@ const Landing = () => {
     <div className="bg-black h-full w-auto overflow-y-hidden">
       <div className="w-full flex ">
         <div className="flex flex-1 text-white px-10 pt-48 flex-col">
-          <p className="text-5xl font-bold text-center text-clip">
-            Know the <span className="text-orange-300">Public Opinion</span> or
-            let know your opinion
-          </p>
-          <p className="text-xl text-center mt-10">
-            We offer a sanctuary for those seeking to navigate the currents of
-            thought and emotion, providing a digital haven where minds converge,
-            ideas flourish, and connections ignite. Within this dynamic
-            ecosystem, individuals are invited to share their insights, pose
-            questions, and explore the myriad pathways that shape their
-            existence. It&apos a place where the ever-changing winds of
-            inspiration guide the collective journey, fostering a sense of
-            community, collaboration, and growth. Here, in the fluidity of
-            discourse and discovery, each participant finds their unique rhythm,
-            charting a course that resonates with their deepest aspirations. As
-            the currents of change ebb and flow, FlowSpace remains a steadfast
-            beacon, illuminating the way forward and inviting all to embrace the
-            transformative power of shared experience.
+        <Image src="/assets/images/Eagl.jpeg" alt="Eagl" width={100} height={20} />
+                  <p className="text-xl text-center mt-10">
+                  Beautiful Journey of creation of EAGL as a social enterprise began with founder directors Ph D research at IITB on “Hunger and Starvation Deaths among Tribes in the Melghat Region of Maharashtra”. Rigorous process of Ph D allowed him to comprehend poverty and issues related to social exclusion of the tribals and began initiative focused on livelihood creation with the objective of eradication of the poverty and making community and nation self-reliance in true sense.
           </p>
           <div className="flex gap-5 justify-center mt-10 items-center">
             <Link className="btn-grad font-bold text-2xl" href="/register">
@@ -75,6 +61,7 @@ const Landing = () => {
         </div>
         <div className="flex flex-1">
           <Lottie options={defaultOptions} height={700} width={700} />
+         {/* <Image src="/assets/images/Goat.jpg" alt="Goat" width={500} height={500}/> */}
         </div>
       </div>
       <div className="w-full grid grid-cols-3 gap-20 px-40 pb-10 text-white mt-56 text-center">
